a1db3b9841e1428019a1878686d06caf05117b66
Razzzan <126163074+Razzzan@users.noreply.github.com>
2024-04-21 16:22:42 +0300

    Update MatrixMultwTile.cu

--- MatrixMultwTile.cu
+++ MatrixMultwTile.cu
@@ -46,60 +46,49 @@ int main(){
     int size_matrixB = N * N * sizeof(float);
     int size_resultMatrix = M * N * sizeof(float);
 
-    // Allocate memory on host
     host_matrixA = (float*)malloc(size_matrixA);
     host_matrixB = (float*)malloc(size_matrixB);
     host_resultMatrix = (float*)malloc(size_resultMatrix);
 
-    // Initialize matrices
+    srand(time(NULL));
     for (int i = 0; i < M; i++) {
         for (int j = 0; j < N; j++) {
-            host_matrixA[i * N + j] = i + j;
+            host_matrixA[i * N + j] =rand() % 10 + 1;
         }
     }
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            host_matrixB[i * N + j] = i - j;
+            host_matrixB[i * N + j] =rand() % 10 + 1;
         }
     }
 
-    // Allocate memory on device
     cudaMalloc(&device_matrixA, size_matrixA);
     cudaMalloc(&device_matrixB, size_matrixB);
     cudaMalloc(&device_resultMatrix, size_resultMatrix);
 
-    // Copy data from host to device
     cudaMemcpy(device_matrixA, host_matrixA, size_matrixA, cudaMemcpyHostToDevice);
     cudaMemcpy(device_matrixB, host_matrixB, size_matrixB, cudaMemcpyHostToDevice);
 
-    // Define grid and block dimensions
     dim3 dimGrid((N + TILE_SIZE - 1) / TILE_SIZE, (M + TILE_SIZE - 1) / TILE_SIZE, 1);
     dim3 dimBlock(TILE_SIZE, TILE_SIZE, 1);
 
-    // Record start time
     cudaEvent_t start, stop;
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
     cudaEventRecord(start);
 
-    // Launch kernel for matrix multiplication
     matrix_multiply<<<dimGrid, dimBlock>>>(device_matrixA, device_matrixB, device_resultMatrix, M, N);
 
-    // Record stop time and synchronize device
     cudaEventRecord(stop);
     cudaEventSynchronize(stop);
 
-    // Calculate elapsed time
     float milliseconds = 0;
     cudaEventElapsedTime(&milliseconds, start, stop);
 
-    // Copy result back to host
     cudaMemcpy(host_resultMatrix, device_resultMatrix, size_resultMatrix, cudaMemcpyDeviceToHost);
 
-    // Print execution time in milliseconds
     printf("Execution time: %.2f ms\n", milliseconds);
 
-    // Free memory
     free(host_matrixA);
     free(host_matrixB);
     free(host_resultMatrix);
